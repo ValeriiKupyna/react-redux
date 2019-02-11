16fc51d65a71120c118538dc3873c6316807dc5c
vkupyna <vkupyna@productiveedge.com>
2019-02-11 13:01:29 +0300

    clean master

--- src/components/app-header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Header = () => <h1>TODO LIST</h1>;
-
-export default Header;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/app-header';
-import AppList from './components/app-list';
-import AppSearch from './components/app-serch';
-
-const todoList = [
-    {label: 'test1', important: true, id: 1},
-    {label: 'test2', important: false, id: 2},
-    {label: 'test3', important: false, id: 3},
-];
 
 const App = () => (
     <div>
-        <Header />
-        <AppSearch />
-        <AppList todoList={todoList}/>
+        Hello Word
     </div>
 );
 
